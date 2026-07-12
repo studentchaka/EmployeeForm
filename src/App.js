65c67e51db795d0922c03b187526c83ef8093aba
Jordanloza <loza333@live.com>
2026-07-12 17:21:06 -0500

    Inital commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import React from "react";
+import EmployeeForm from "./EmployeeForm";
+
+function App() {
+  return (
+    <div className="App">
+      <EmployeeForm />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/EmployeeForm.js
@@ -0,0 +1,88 @@
+import React from "react";
+import "./EmployeeForm.css";
+
+class EmployeeForm extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      name: "",
+      email: "",
+      title: "",
+      department: ""
+    };
+  }
+
+  handleChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  };
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+
+    console.log(this.state);
+
+    this.setState({
+      name: "",
+      email: "",
+      title: "",
+      department: ""
+    });
+  };
+
+  render() {
+    return (
+      <div className="employee-form-container">
+        <h2>New Employee Form</h2>
+
+        <form onSubmit={this.handleSubmit}>
+          <div>
+            <label>Name:</label>
+            <input
+              type="text"
+              name="name"
+              value={this.state.name}
+              onChange={this.handleChange}
+            />
+          </div>
+
+          <div>
+            <label>Email:</label>
+            <input
+              type="email"
+              name="email"
+              value={this.state.email}
+              onChange={this.handleChange}
+            />
+          </div>
+
+          <div>
+            <label>Job Title:</label>
+            <input
+              type="text"
+              name="title"
+              value={this.state.title}
+              onChange={this.handleChange}
+            />
+          </div>
+
+          <div>
+            <label>Department:</label>
+            <input
+              type="text"
+              name="department"
+              value={this.state.department}
+              onChange={this.handleChange}
+            />
+          </div>
+
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default EmployeeForm;
